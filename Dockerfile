83ea10c1d4876bfbcd43fa839606b2762192f9d4
Jasper Stam <jasperstam1@gmail.com>
2020-02-12 16:40:00 +0100

    remove stopwords, list not yet complete. Remove dot, comma and empty elements.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3
 
- ADD entry_script.py /
- ADD requirements.txt /
- ADD dataset-1/ /input/
+ADD entry_script.py /
+ADD requirements.txt /
+ADD dataset-1/ /input/
+ADD stopwords.txt /
 
- RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt
 
- ENTRYPOINT [ "python", "./entry_script.py", "1" ]
+ENTRYPOINT [ "python", "./entry_script.py", "1" ]
--- entry_script.py
+++ entry_script.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import os
 import time
+import re
 
 
 def write_output_file():
@@ -37,11 +38,32 @@ def read_input_file(ifile):
         csv_reader = csv.reader(csvfile, delimiter=",")
         reqs = {}
         line_count = 0
+        spacesre = re.compile(r'^\s*$')
         for row in csv_reader:
             if line_count > 0:
-                reqs[row[0]] = row[1].split(" ")
+                reqs[row[0]] = [i for i in row[1].lower().replace(".", "").replace(",", "").split(" ") if not spacesre.match(i)]
             line_count += 1
         return reqs
+
+def get_stopwords():
+    try:
+        stopfile = open("stopwords.txt", 'r')
+    except IOError:
+        print("Error: File with stopwords does not exist.")
+        return 0
+
+    stopwords = stopfile.read().lower().splitlines()
+    stopfile.close()
+    return stopwords
+
+def remove_stop_words(inreqs):
+    stopwords = get_stopwords()
+    outreqs = {}
+
+    for k, v in inreqs.items():
+        outreqs[k] = [x for x in v if x not in stopwords]
+
+    return outreqs
 
 
 if __name__ == "__main__":
@@ -63,11 +85,14 @@ if __name__ == "__main__":
     print(f"Hello world, running with matchtype {match_type}!")
 
     # Read input low-level requirements and count them (ignore header line).
-    with open("/input/low.csv") as inputfile:
+    with open("/input/low.csv", 'r') as inputfile:
         print(f"There are {len(inputfile.readlines()) - 1} low-level requirements")
 
     lowreqs = read_input_file("/input/low.csv")
-    print(lowreqs)
+    # print(lowreqs)
+
+    lowrtrun = remove_stop_words(lowreqs)
+    print(lowrtrun)
 
     write_output_file()
     time.sleep(15)
